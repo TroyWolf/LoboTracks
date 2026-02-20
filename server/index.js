ace3961b23d210b42e56b37a69ede05ee5a42d88
Troy Wolf <troy@troywolf.com>
2026-02-20 07:52:47 -0600

    client build is in dist, not build

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 }
 
 /**
@@ -206,7 +206,7 @@ app.get('/api/tracks/:filename', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
   );
 }
 
